824292bd2ecf2d214b77b9de91bd38ee51f517f3
Simodiri <dirienzo.1844531@studenti.uniroma1.it>
2021-07-15 12:08:35 +0200

    first commit

--- catkin_ws/src/progetto/src/progetto.cpp
+++ catkin_ws/src/progetto/src/progetto.cpp
@@ -17,6 +17,7 @@ float mod_vel_x=0;
 float mod_vel_y=0;
 float angular=0;
 bool vel_mod=false;
+float target_x=0;
 using namespace Eigen;
 
 void VelModCallBack(const geometry_msgs::Twist::ConstPtr& msg){
@@ -26,8 +27,11 @@ vel_mod=true; //è arrivato un comando
  mod_vel_y=msg->linear.y;
  angular=msg->angular.z;
 
+ target_x=mod_vel_x*0.2;
+ //target_y=vel_y*0.2;
 }
 void LaserCallBack_aux(const sensor_msgs::LaserScan::ConstPtr& scan_in){
+   //devo estrarre i punti dal Laser_scan
    laser_geometry::LaserProjection projector_;
    sensor_msgs::PointCloud cloud;
    tf::TransformListener listener_;
@@ -60,21 +64,24 @@ try
        //prendiamo l'opposto delle forze applicate perchè hanno stessa direzioni ma verso opposto dei robot e ostacolo
        sum_fx=-sum_fx;
        sum_fy=-sum_fy;
-       sum_fx*=abs(mod_vel_x)/600;
-       sum_fy*=abs(mod_vel_y)/600;
+        geometry_msgs::Twist send;
+       float distanza=sqrt(target_x*target_x+0*0); // è la distanza dal punto in cui voglio far ruotare il robot
+       ROS_INFO("La distanza è %f \n", distanza);
        ROS_INFO("Le forze sono %f %f %f\n", sum_fx,sum_fy);
-         
-       geometry_msgs::Twist send;
-       float distanza=sqrt(mod_vel_x*mod_vel_x+mod_vel_y*mod_vel_y);
-       send.linear.x=sum_fx+mod_vel_x;
-       if(send.linear.x>0 && send.linear.x<0.1 || sum_fx>1){
-        send.angular.z=1;
-      }else{
-        send.angular.z=angular;
+        send.angular.z=distanza*sum_fy/30;
+        //agisco sulle componenti x ed y della velocità
+        sum_fx*=abs(mod_vel_x)/500; 
+        sum_fy*=abs(mod_vel_y)/500;
 
-      }
+       send.linear.x= sum_fx + mod_vel_x;
+       //if(send.linear.x>0 && send.linear.x<0.1 || sum_fx>1){
+       // send.angular.z=1;
+      //}else{
+        send.angular.z+=angular;
+        // }
+       
+       send.linear.y=mod_vel_y+sum_fy;
       
-       //send.linear.y=mod_vel_y+sum_fy;
        
        ROS_INFO("Nuovo vettore velocità %f %f %f\n", send.linear.x,send.linear.y,send.angular.z);
           
@@ -103,10 +110,11 @@ int main(int argc, char **argv){
    
    ros::Subscriber vel_sub=n.subscribe("/cmd_vel_mod",1000,VelModCallBack);// subscriber per il comando di velocità modificato
    ROS_INFO("Ho avviato il subscriber su /cmd_vel_mod") ;
+    ROS_INFO("ciao") ;
 
    ROS_INFO("Inviare al topic /cmd_vel_mod il comando per far muovere il robot");
    ros::spin();
-
+   
   return 0;
 
 
